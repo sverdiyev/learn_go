f337f1f6221c5792a12ff1d916010a2591e00ff5
sverdiyev <oleksandr.verdiyev@devbridge.com>
2022-11-03 23:29:14 +0100

    best practice - pass by reference

--- main.go
+++ main.go
@@ -2,24 +2,39 @@ package main
 
 import "fmt"
 
+type Animal interface {
+	Says() string
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Cat struct {
+	Name  string
+	Color string
+}
+
+//Interface is implemented because the type implements its methods
+//Not because the type implements the interface
+
+// Best practice - pass the reference
+
+func (d *Dog) Says() string {
+	return "My name is " + d.Name
+}
+func (c *Cat) Says() string {
+	return "My name is " + c.Name
+}
+
+func introduce(animal Animal) {
+	fmt.Println(animal.Says())
+}
+
 func main() {
-
-	//loop over map
-	myMap := map[string]string{"me": "Sasha", "you": "unknown", "him": "my Boi"}
-
-	for key, value := range myMap {
-		fmt.Println(key, value)
-	}
-
-	fmt.Println()
-	fmt.Println()
-
-	//loop over slice
-	mySlice := []string{"Sasha", "Masha"}
-	mySlice = append(mySlice, "Go", "Bravo", "Gopher", "Alpha", "Grin", "Delta")
-
-	for i, myString := range mySlice {
-		fmt.Println(i, myString)
-	}
-
+	myDog := Dog{"DOG", "Boiboi"}
+	notMyCat := Cat{"CAT", "girgirl"}
+	introduce(&myDog)
+	introduce(&notMyCat)
 }
