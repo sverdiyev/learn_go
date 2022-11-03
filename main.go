0f73baf9c10fca04c7d2851aabb6fc0cd6fc9dcd
sverdiyev <oleksandr.verdiyev@devbridge.com>
2022-11-03 20:55:13 +0100

    struct with methods

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	FirstName string
+	LastName  string
+	BirthDate time.Time
+}
+
+func (user *User) printFullName() {
+	fmt.Println(user.FirstName + " " + user.LastName)
+}
+func (user *User) getFullName() string {
+	return user.FirstName + " " + user.LastName
+}
+
 func main() {
+
+	user := User{
+		FirstName: "Sasha",
+		LastName:  "Ver",
+		BirthDate: time.Time{},
+	}
+	fmt.Println(user)
+
+	user.printFullName()
+
 }
